f63e60b37b8631f57305e22aa2e48030758e3d28
OP <106511969+Op-Pautu@users.noreply.github.com>
2025-02-06 11:49:19 +0530

    feat: enhance brand retrieval with pagination and query validation

--- op-medusa/src/api/admin/brands/route.ts
+++ op-medusa/src/api/admin/brands/route.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-import { MedusaRequest, MedusaResponse } from "@medusajs/framework";
+import { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
 import { PostAdminCreateBrand } from "src/api/admin/brands/validators";
 import { createBrandWorkflow } from "src/workflows/create-brand";
 
@@ -17,13 +17,34 @@ export const POST = async (
   res.json({ brand: result });
 };
 
+//
+
 export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
   const query = req.scope.resolve("query");
-
-  const { data: brands } = await query.graph({
+  const { data: brands, metadata } = await query.graph({
     entity: "brand",
-    fields: ["*", "products.*"],
+    ...req.queryConfig,
   });
 
-  res.json({ brands });
+  const count = metadata?.count || 0;
+  const take = metadata?.take || 0;
+  const skip = metadata?.skip || 0;
+
+  res.json({
+    brands,
+    count,
+    limit: take,
+    offset: skip,
+  });
 };
+
+// export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
+//   const query = req.scope.resolve("query");
+
+//   const { data: brands } = await query.graph({
+//     entity: "brand",
+//     fields: ["*", "products.*"],
+//   });
+
+//   res.json({ brands });
+// };
